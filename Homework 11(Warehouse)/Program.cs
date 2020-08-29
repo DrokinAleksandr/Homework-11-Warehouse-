25b565fb93073b5b4453ed808db69e28056f4592
Aleksandr Drokin <alexajaxmakarenko@gmail.com>
2020-08-29 11:39:33 +0300

    add lib

--- Homework 11(Warehouse)/Program.cs
+++ Homework 11(Warehouse)/Program.cs
@@ -3,13 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WarehouseLib;
 
-namespace Homework_11_Warehouse_
+namespace Homework_Warehouse_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            String nameProduct, continueProg;
+            int menuNumber, priceProduct, baseProduct;
+
+            continueProg = "yes";
+            while (continueProg == "yes")
+            {
+                Console.WriteLine("=========Warehouse=========");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1 Add Product\n2 Wiev All Product\n3 Exit\n");
+                Console.Write("Select number: ");
+                menuNumber = CheckInt();
+
+                switch (menuNumber)
+                {
+                    case 1:
+                        Console.WriteLine("Please fill the data of your product");
+                        Console.Write("Enter the Name: ");
+                        nameProduct = Console.ReadLine();
+
+                        Console.Write("Enter the Price: ");
+                        priceProduct = CheckInt();
+
+                        Console.Write("Enter the Base(shelf life): ");
+                        baseProduct = CheckInt();
+
+                        string guid = System.Guid.NewGuid().ToString();
+
+
+                        prodData DataAboutProduct = new prodData(newID: guid, nameProd: nameProduct, priceProd: priceProduct, baseProd: baseProduct);
+
+                        productCollection.Add(DataAboutProduct);
+                        Console.WriteLine("==============================");
+                        Console.WriteLine("Product has been enterd!");
+                        Console.WriteLine("Do you want continue?");
+                        Console.Write("Write answer(yes/no): ");
+                        continueProg = Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+
+                    case 2:
+                        break;
+
+
+                    case 3:
+                        break;
+                }
+
+            }
+            Console.WriteLine("THE END");
+            Console.ReadKey();
+        }
+
+        static int CheckInt()
+        {
+            int checkResultInt;
+
+            while (!int.TryParse(Console.ReadLine(), out checkResultInt))
+            {
+                Console.Write("Error, please write number: ");
+            }
+
+            return checkResultInt;
         }
     }
 }
---
+++ WarehouseLib/WarehouseLib.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseLib
+{
+    public static class productCollection
+    {
+        private static List<prodData> _collProd = new List<prodData>();
+        public static void Add(prodData DataAboutProduct)
+        {
+            _collProd.Add(DataAboutProduct);
+        }
+
+        public static prodData[] GetProdData()
+        {
+            return _collProd.ToArray();
+        }
+    }
+    public class prodData
+    {
+        public prodData(string newID, string nameProd, int priceProd, int baseProd)
+        {
+            this.ProdID = newID;
+            this.ProdName = nameProd;
+            this.ProdPrice = priceProd;
+            this.ProdBase = baseProd;
+            //this.shelfLife = ;
+        }
+
+        public string ProdID { get; set; }
+        public string ProdName { get; set; }
+        public int ProdPrice { get; set; }
+        public int ProdBase { get; set; }
+    }
+    public class shelfLife
+    {
+        public shelfLife(int baseProd)
+        {
+            DateTime shelfLifeProd;
+            shelfLifeProd = DateTime.Today.AddDays(baseProd);
+            return;
+        }
+
+    }
+}
